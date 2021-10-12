3b8b57951a24fc3694f236b3f8f197540e63d4c2
Tássio <tassio72@hotmail.com>
2021-10-12 17:26:07 -0300

    feat: create building component

---
+++ pages/building/index.vue
@@ -0,0 +1,158 @@
+<template>
+  <section class="container-view">
+    <div class="header">
+      <h1 class="header--title tj-text-panel">
+        We are building something awesome for you! ;)
+      </h1>
+    </div>
+    <BuildingSVG />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import BuildingSVG from '@/components/Pages/Building/BuildingSVG.vue'
+
+export default Vue.extend({
+  name: 'Posts',
+  components: {
+    BuildingSVG,
+  },
+  layout: 'blog',
+})
+</script>
+
+<style lang="scss" scoped>
+@import '~@/styles/layouts/blog/index.scss';
+
+.header {
+  margin-bottom: $space-lg;
+}
+
+.content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+svg {
+  width: calc(68px * 3);
+  height: calc(66px * 3);
+}
+
+/* ELEMENTS POSITIONS */
+#building_bottom,
+#window_01,
+#window_02,
+#door {
+  transform: translateY(20px);
+  z-index: 1;
+}
+
+#building_top,
+#balcony_01,
+#balcony_02,
+#balcony_03,
+#balcony_04,
+#balcony_05,
+#balcony_06 {
+  transform: translateY(56px);
+  z-index: -1;
+}
+
+#small_tree,
+#big_tree {
+  transform: translateY(28px);
+}
+
+/* ELEMENTS ANIMATIONS */
+#building_bottom {
+  animation: buildingBottom 1s ease-out forwards;
+}
+
+#window_01,
+#window_02,
+#door {
+  animation: buildingBottom 1s 0.1s ease-in forwards;
+}
+
+#building_top {
+  animation: buildingTop 1s 1.2s ease-out forwards;
+}
+
+#balcony_01,
+#balcony_02,
+#balcony_03 {
+  animation: buildingTop 1s 1.4s ease-out forwards;
+}
+
+#balcony_04,
+#balcony_05,
+#balcony_06 {
+  animation: buildingTop 1s 1.6s ease-out forwards;
+}
+
+#small_tree,
+#big_tree {
+  animation: trees 1s 2s ease-out forwards;
+}
+
+#cloud_left {
+  transform: translateX(-11px);
+  animation: cloudLeft 20s 2s linear infinite;
+}
+
+#cloud_right {
+  transform: translateX(72px);
+  animation: cloudRight 15s 2s linear infinite;
+}
+
+/* ANIMATIONS */
+@keyframes buildingBottom {
+  0% {
+    transform: translateY(20px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes buildingTop {
+  0% {
+    transform: translateY(56px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes trees {
+  0% {
+    transform: translateY(28px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes cloudLeft {
+  0% {
+    transform: translateX(-11px);
+  }
+  100% {
+    transform: translateX(100px);
+  }
+}
+
+@keyframes cloudRight {
+  0% {
+    transform: translateX(72px);
+  }
+  100% {
+    transform: translateX(-100px);
+  }
+}
+</style>
