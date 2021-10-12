bae84d0fa5a4b3943adabf9bfc06ca559f980ae6
Tássio <tassio72@hotmail.com>
2021-10-12 09:59:21 -0300

    chore: creating nice footer

--- components/Layouts/Blog/Footer.vue
+++ components/Layouts/Blog/Footer.vue
@@ -1,6 +1,42 @@
 <template>
-  <VFooter :absolute="!fixed" app>
-    <span>&copy; {{ new Date().getFullYear() }}</span>
+  <VFooter :absolute="!fixed" dark padless>
+    <VCard class="flex" flat tile>
+      <VCardTitle class="teal">
+        <strong class="tj-text-subheading"
+          >Let's getting in touch on social networks!</strong
+        >
+
+        <VSpacer></VSpacer>
+
+        <VBtn
+          v-for="social in socials"
+          :key="social.icon"
+          :href="social.link"
+          class="footer--social__link"
+          target="_blank"
+          dark
+          icon
+        >
+          <VIcon size="24px">
+            {{ social.icon }}
+          </VIcon>
+        </VBtn>
+      </VCardTitle>
+
+      <VCardText class="py-2 white--text text-center">
+        Made with ❤️by
+        <VBtn
+          class="footer--open-ish__link"
+          small
+          plain
+          color="secondary"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/open-ish"
+          ><strong>Open-ish</strong>
+        </VBtn>
+      </VCardText>
+    </VCard>
   </VFooter>
 </template>
 
@@ -11,7 +47,31 @@ export default Vue.extend({
   data() {
     return {
       fixed: false,
+      socials: [
+        { link: 'https://github.com/tassioFront', icon: 'mdi-github' },
+        {
+          link: 'https://www.linkedin.com/in/tassio-front-end/',
+          icon: 'mdi-linkedin',
+        },
+      ],
     }
   },
 })
 </script>
+
+<style lang="scss" scoped>
+@import '~@/styles/layouts/blog/index.scss';
+$text-gap: -10px;
+
+.footer {
+  &--social__link {
+    &:not(:first-child) {
+      margin-right: $space-sm;
+    }
+  }
+
+  &--open-ish__link {
+    margin-left: $text-gap;
+  }
+}
+</style>
