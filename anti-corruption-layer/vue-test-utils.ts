0d1ac5ce885df378de0bdb87bd3b702ea17eda32
Tássio <tassio72@hotmail.com>
2021-10-12 11:00:03 -0300

    tests: create anti corruption layer

---
+++ anti-corruption-layer/vue-test-utils.ts
@@ -0,0 +1,37 @@
+import {
+  mount,
+  shallowMount,
+  createLocalVue,
+  ThisTypedMountOptions,
+  VueClass,
+} from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import VueHtmlSecure from 'vue-html-secure'
+
+const localVue = createLocalVue()
+Vue.use(Vuetify)
+localVue.use(VueHtmlSecure)
+
+const defaultProperties = () => {
+  return {
+    localVue,
+    vuetify: new Vuetify(),
+  }
+}
+
+export function testSetup(
+  component: VueClass<Vue>,
+  customConfig: ThisTypedMountOptions<Vue>,
+  shallow = false
+) {
+  return shallow
+    ? shallowMount(component, {
+        ...defaultProperties(),
+        ...customConfig,
+      })
+    : mount(component, {
+        ...defaultProperties(),
+        ...customConfig,
+      })
+}
