e20e7d975a1ebd1e139c5311b2f34ddc5c69ab63
Tássio <tassio72@hotmail.com>
2021-10-24 16:32:32 -0300

    chore: update about

--- fakeDataBase/about-new.ts
+++ fakeDataBase/about-new.ts
@@ -1,52 +1,62 @@
-import { keepReadings } from './hello'
-import { IAboutViewContent } from '~/models/Blog/About/IAboutViewContent'
-
-const INTRODUCTION: IAboutViewContent = {
+const INTRODUCTION = {
   id: 'intro',
   title: 'Introduction',
   body: [
     {
-      text: keepReadings[0].body,
+      type: 'LongText',
+      value: `Hi, I'm Tássio Jordão (TJ), a Chemical with Front-End experience.
+          Actually, I'm a Frontend developer with some Chemistry knowledge. That
+          decision (changing from chemistry to coding) was in 2018 when I had
+          contact with VBA (programming language) in an old professional
+          experience - for those who don't know VBA is to do automatic tasks in
+          Excel - , at that moment I noticed as I love coding. After talking with
+          some friends from this area, I decided to become a Front End developer.
+          It was in 2019 January. And, after 6 months of studying hard, I got my
+          first opportunity on a website about recipes. It was an awesome
+          achievement and fired up me to keep in this new area.`,
     },
   ],
 }
 
-const BREAK_ICE: IAboutViewContent = {
+const BREAK_ICE = {
   id: 'break-ice',
   title: 'Just to break the ice - what I do for fun',
   body: [
     {
-      text: `For sure that's a good one! So, I love wines🍷.`,
+      type: 'SimpleText',
+      value: `For sure that's a good one! So, I love wines🍷.`,
     },
     {
-      text: `It's a recent love and
+      type: 'LongText',
+      value: `It's a recent love and
   hobby, which happened when my wife and I went to a winery here in São
   Paulo. Since now, I like to read about wines and share them with
   friends. And yeah, catching up with friends is another one!`,
     },
     {
-      text: `I love
+      type: 'SimpleText',
+      value: `I love
   pets too, dogs to be more specific. I like to travel with my wife. We
   like to know calm places and to have an awesome breakfast.`,
     },
   ],
 }
 
-const HARD_SKILLS: IAboutViewContent = {
+const HARD_SKILLS = {
   id: 'hard-skills',
   title: 'Hard Skills',
   body: [
     {
-      type: 2,
+      type: 'FriendlyList',
       title: 'I have been working with:',
-      content: [
+      value: [
         {
           text: 'HTML',
-          color: 'purple darken-1',
+          color: 'purple',
         },
         {
           text: 'CSS',
-          color: 'light-blue darken-1',
+          color: 'light-blue',
         },
         {
           text: 'JavaScript',
@@ -54,31 +64,31 @@ const HARD_SKILLS: IAboutViewContent = {
         },
         {
           text: 'Vue 2 (Vuex and VueRouter)',
-          color: 'green darken-1',
+          color: 'green',
         },
 
         {
           text: 'Sass',
-          color: 'pink darken-1',
+          color: 'pink',
         },
         {
           text: 'TypeScript',
-          color: 'blue darken-1',
+          color: '#039BE5FF',
         },
         {
           text: 'Jest',
-          color: 'deep-orange darken-1',
+          color: 'deep-orange',
         },
         {
           text: 'Vuetify',
-          color: 'light-blue darken-1',
+          color: 'light-blue',
         },
       ],
     },
     {
-      type: 2,
+      type: 'FriendlyList',
       title: 'I worked with:',
-      content: [
+      value: [
         {
           text: 'Tailwind',
           color: 'teal darken-1',
@@ -90,9 +100,9 @@ const HARD_SKILLS: IAboutViewContent = {
       ],
     },
     {
-      type: 2,
+      type: 'FriendlyList',
       title: 'I had contact in personal projects',
-      content: [
+      value: [
         {
           text: 'React',
           color: 'deep-purple darken-1',
@@ -114,49 +124,50 @@ const HARD_SKILLS: IAboutViewContent = {
   ],
 }
 
-const SOFT_SKILLS: IAboutViewContent = {
+const SOFT_SKILLS = {
   id: 'soft-skills',
   title: 'Soft Skills',
   body: [
     {
-      text: `I'm an easy-going person who likes to share what I have been learning.`,
+      type: 'SimpleText',
+      value: `I'm an easy-going person who likes to share what I have been learning.`,
     },
     {
-      type: 2,
-      content: [
+      type: 'FriendlyList',
+      value: [
         {
           text: 'User Experience first',
-          color: 'cyan lighten-1',
+          color: '#26C6DAFF',
           contrast: 'black',
           icon: 'mdi-clipboard-account',
         },
         {
           text: 'Effective listener',
-          color: 'cyan lighten-1',
+          color: '#26C6DAFF',
           contrast: 'black',
           icon: 'mdi-account-tie-voice',
         },
         {
           text: 'Clear communication',
-          color: 'cyan lighten-1',
+          color: '#26C6DAFF',
           contrast: 'black',
           icon: 'mdi-account-multiple',
         },
         {
           text: 'Attentive',
-          color: 'cyan lighten-1',
+          color: '#26C6DAFF',
           contrast: 'black',
           icon: 'mdi-phone-outline',
         },
         {
           text: 'Professional',
-          color: 'cyan lighten-1',
+          color: '#26C6DAFF',
           contrast: 'black',
           icon: 'mdi-account-tie',
         },
         {
           text: 'Empathetic',
-          color: 'cyan lighten-1',
+          color: '#26C6DAFF',
           contrast: 'black',
           icon: 'mdi-hand-heart-outline',
         },
@@ -165,45 +176,50 @@ const SOFT_SKILLS: IAboutViewContent = {
   ],
 }
 
-const EXPERIENCES: IAboutViewContent = {
+const EXPERIENCES = {
   id: 'experiences',
   title: 'My experiences',
   body: [
     {
-      type: 3,
-      content: [
+      type: 'PeriodList',
+      value: [
         {
           text: '<strong>Frontend Engineer</strong> @Juntos Somos Mais',
           caption: 'Since 2020 Mar',
+          color: '#26C6DAFF',
         },
         {
           text: '<strong>Frontend Engineer</strong> @CyberCook',
           caption: '2019 Jun to 2020 Mar',
+          color: '#26C6DAFF',
         },
         {
           text: '<strong>Sensory Analyst and Information Technology</strong> @Sensenova',
           caption: 'Oct 2017 to Jun 2019',
+          color: '#26C6DAFF',
         },
         {
           text: '<strong>Intern (chemistry)</strong> @Givaudan Brazil',
           caption: 'Jul 2016 to Sep 2017',
+          color: '#26C6DAFF',
         },
         {
           text: '<strong>Laboratory technician and information technology assistant</strong> @Santo Ivo School',
           caption: 'Jan 2014 to Jul 2016',
+          color: '#26C6DAFF',
         },
         {
           text: '<strong>Intern (information technology)</strong> @FUNDAP',
           caption: 'Feb 2012 to Feb 2013 ',
+          color: '#26C6DAFF',
         },
       ],
     },
   ],
 }
-export const contents: IAboutViewContent[] = [
-  INTRODUCTION,
-  BREAK_ICE,
-  HARD_SKILLS,
-  SOFT_SKILLS,
-  EXPERIENCES,
-]
+
+export const data = {
+  title: 'About me',
+  intro: 'Please, get a coffee and make yourself at home.',
+  contents: [INTRODUCTION, BREAK_ICE, HARD_SKILLS, SOFT_SKILLS, EXPERIENCES],
+}
